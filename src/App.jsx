84a5130966055ae8bd0e27a7ed074bb7480cfaff
TRJUNED5 <trjuned2@gmail.com>
2025-04-12 00:34:51 -0400

    "create pages and routes"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <h1 className='text-red-400'>
-      APP
-    </h1>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/sign-in" element={<Signin/>}/>
+      <Route path="/sign-up" element={<SignUp />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/profile" element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
