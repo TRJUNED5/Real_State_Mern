9ee769656ad3a238bf8920c15a5f6e5d36ba6e63
TRJUNED5 <trjuned2@gmail.com>
2025-05-17 12:05:01 -0400

    "header components"

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignUp from './pages/SignUp'
 import Signin from './pages/Signin'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/sign-in" element={<Signin/>}/>
