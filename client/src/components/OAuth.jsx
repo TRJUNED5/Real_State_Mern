086a6bbdb4aeed720157c6a5b412ee927abc2712
TRJUNED5 <trjuned2@gmail.com>
2025-09-19 13:04:35 -0400

    "oAuth"

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,9 +3,11 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
 import { app } from '../firebase'
 import { useDispatch } from 'react-redux'
 import { signInSuccess } from '../redux/user/userSlice'
+import { useNavigate } from 'react-router-dom'
 
 export default function OAuth() {
   const dispatch = useDispatch();
+    const navigate = useNavigate();
   const handleGoogleClick = async() => {
     try {
       const provider = new GoogleAuthProvider();
@@ -16,13 +18,14 @@ export default function OAuth() {
       const res = await fetch('/api/auth/google', {
         method: 'POST',
         headers: {
-          'Contetnt-Type': 'application/json',
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({ name: result.user.displayName, email: result.user.email, photo: result.user.photoURL}),
 
       })
       const data = await res.json();
       dispatch(signInSuccess(data));
+        navigate('/')
 
     } catch (error) {
       console.log('could not sign in with google',error)
