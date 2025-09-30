94e16c3d68d840b85d5cef312f385ac7375cf1a7
TRJUNED5 <trjuned2@gmail.com>
2025-09-29 23:43:35 -0400

    updatefunctionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import { useRef } from 'react'
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase'
+import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice'
 
 export default function Profile() { 
   const fileRef = useRef(null) // for image uploading from image
@@ -11,7 +12,6 @@ export default function Profile() {
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
-  console.log(formData)
 
   //firebase storage
   // allow read;
@@ -42,18 +42,32 @@ export default function Profile() {
             setFileUploadError(true);
           },
           ()=>{
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => setFormData({...formData, avatar: downloadURL})
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => 
+              setFormData({...formData, avatar: downloadURL})
           
           )
             
           }
         )
+    };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value})
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      
+    } catch (error) {
+      
     }
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input onChange={(e)=>setfile(e.target.files[0])} type="file" ref={fileRef} hidden accepts='image/*'/> {/*Only image will be accepted and using reference for selecting image from profile image*/}
         <img onClick={()=>fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
         <p className='text-sm self-center'>
@@ -70,9 +84,27 @@ export default function Profile() {
             
           )}
         </p>
-        <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg'/>
-        <input type='email' placeholder='email' id='email' className='border p-3 rounded-lg'/> 
-        <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg'/>
+        <input 
+          type='text'
+          placeholder='username'
+          defaultValue={currentUser.username} 
+          id='username' 
+          className='border p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input 
+          type='email' 
+          placeholder='email'
+          defaultValue={currentUser.email} 
+          id='email' 
+          className='border p-3 rounded-lg'
+          onChange={handleChange}
+        /> 
+        <input 
+          type='password' 
+          placeholder='password' 
+          id='password' 
+          className='border p-3 rounded-lg'/>
         <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>update</button>
       </form>
 
