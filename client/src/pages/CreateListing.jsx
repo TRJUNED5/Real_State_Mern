9ec6be464049d757f1b2d3a44a17c2b9d9a541cd
TRJUNED5 <trjuned2@gmail.com>
2025-10-13 10:53:30 -0400

    complete image upload functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { 
+    getDownloadURL, 
+    getStorage, ref, 
+    uploadBytesResumable 
+} from 'firebase/storage';
 import { app } from '../firebase';
 
 export default function CreateListing() {
@@ -8,9 +12,12 @@ const [formData, setFormData] = useState({
     imageUrls: [],
 });
 const [imageUploadError, setImageUploadError] = useState(false);
+const [uploading, setUploading] = useState(false);
 console.log(formData);
 const handleImageSubmit = (e) => {
      if(files.length > 0 && files.length + formData.imageUrls.length < 7){
+        setUploading(true);
+        setImageUploadError(false);
         const promises = [];
 
         for(let i = 0; i < files.length; i++) {
@@ -20,11 +27,14 @@ const handleImageSubmit = (e) => {
             setFormData({ ...formData, imageUrls: formData.imageUrls.concat(urls),
             });
             setImageUploadError(false);
+            setUploading(false);
         }).catch((err) => {
             setImageUploadError('Image upload failed (2 mb max per image)');
+            setUploading(false);
         });
      }else{
-        setImageUploadError('You can only upload 6 per listing')
+        setImageUploadError('You can only upload 6 per listing');
+        setUploading(false);
      }
 };
 
@@ -51,6 +61,13 @@ const handleImageSubmit = (e) => {
             }
         )
     })
+  }
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+        ...formData,
+        imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
   }
   return (
     <main className='p-3 max-w-4xl mx-auto'>
@@ -120,14 +137,30 @@ const handleImageSubmit = (e) => {
                     <span className='font-normal text-gray-600 ml-2'>The first image will be the cover (max 6)</span>
                 </p>
                 <div className='flex gap-4'>
-                    <input onChange={(e)=> setFiles(e.target.files)} className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple/>
-                    <button type='button' onClick={handleImageSubmit} className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+                    <input 
+                      onChange={(e)=> setFiles(e.target.files)} 
+                      className='p-3 border border-gray-300 rounded w-full' 
+                      type="file" 
+                      id='images' 
+                      accept='image/*' 
+                      multiple
+                      />
+                    <button 
+                      type='button'
+                      disabled={uploading} 
+                      onClick={handleImageSubmit} 
+                      className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg 
+                      disabled:opacity-80'
+                      >
+                        {uploading ? 'Uploading...' : 'Upload'}
+                      </button>
                 </div>
                 <p className="text-red-700 text-sm">{imageUploadError && imageUploadError}</p>
                 {
-                    formData.imageUrls.length > 0 && formData.imageUrls.map((url) =>(
-                        <div className="">
-                            <img src={url} alt="listing"/>
+                    formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) =>(
+                        <div key={url} className="flex justify-between p-3 border items-center">
+                            <img src={url} alt="listing image" className="w-20 h-20 object-contain rounded-lg"/>
+                            <button type='button' onClick={() => handleRemoveImage(index)} className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75">Delete</button>
                         </div>
                     ))
                 }
