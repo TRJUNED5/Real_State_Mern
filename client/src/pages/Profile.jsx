e55598eb73a7f3e55f531ac55134bffb0325b10e
TRJUNED5 <trjuned2@gmail.com>
2025-11-05 13:21:35 -0500

    listing page cont

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] =useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   //firebase storage
@@ -117,6 +119,22 @@ export default function Profile() {
     }
   }
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  } 
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -168,6 +186,16 @@ export default function Profile() {
       </div>
       <p className='text-red-700 mt-5'>{error ? error: ''}</p>
       <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully' : ''}</p>
+      <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
+      <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing listings' : ''}</p>
+      {userListings && 
+        userListings.length > 0 && 
+        userListings.map((listing) => 
+        <div key={listing._id} className=''>
+
+        </div>
+      )
+      }
     </div>
   );
 }
