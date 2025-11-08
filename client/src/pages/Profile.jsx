83f116e76dc6e6286ca41708f53375700798d0a7
TRJUNED5 <trjuned2@gmail.com>
2025-11-08 12:00:53 -0500

    complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -190,16 +190,27 @@ export default function Profile() {
       <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing listings' : ''}</p>
       {userListings && 
         userListings.length > 0 && 
-        userListings.map((listing) => 
+        <div className='flex flex-col gap-4'>
+            <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+            {userListings.map((listing) => 
         <div key={listing._id} 
-         className=''>
+         className='border rounded-lg p-3 flex justify-between items-center gap-4'>
           <Link to={`/listing/${listing._id}`}>
-            <img src ={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain rounded-lg'/>
+            <img src ={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain'/>
+          </Link>
+          <Link className='text-slate-700 font-semibold hover:ubderline truncate flex-1' to={`/Listing/${listing._id}`}>
+            <p>{listing.name}</p>
           </Link>
 
+          <div className='flex flex-col item-center'>
+            <button className='text-red-700 uppercase'>Delete</button>
+            <button className='text-green-700 uppercase'>Edit</button>
+          </div>
         </div>
-      )
-      }
+        
+            )}
+      
+        </div>}
     </div>
   );
 }
