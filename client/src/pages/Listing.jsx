4a7fa5638d9c1aeb27384082e21af0a8f1618fd7
TRJUNED5 <trjuned2@gmail.com>
2025-12-31 16:23:25 -0500

    starting

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+
+export default function Listing() {
+    useEffect(()=>{
+        const fetchListing = async () => {
+
+        }
+        fetchListing()
+    })
+    return(
+        <div>Listing</div>
+    )
+}
